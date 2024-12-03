5955c083be2a3e930b8921f225e1c9e78cfd21c0
austinhallett <austin.hallett.doughty@gmail.com>
2024-12-03 13:16:35 -0500

    complete part 2

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -31,6 +31,25 @@
 
 // Analyze the unusual data from the engineers. How many reports are safe?
 
+// --- Part Two ---
+// The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+// The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+// Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+// More of the above example's reports are now safe:
+
+// 7 6 4 2 1: Safe without removing any level.
+// 1 2 7 8 9: Unsafe regardless of which level is removed.
+// 9 7 6 2 1: Unsafe regardless of which level is removed.
+// 1 3 2 4 5: Safe by removing the second level, 3.
+// 8 6 4 4 1: Safe by removing the third level, 4.
+// 1 3 6 7 9: Safe without removing any level.
+// Thanks to the Problem Dampener, 4 reports are actually safe!
+
+// Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+
 package main
 
 import (
@@ -40,39 +59,49 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/austinhallett/advent-of-code/2024/utils"
 )
 
 type Report struct {
 	Levels []int
 }
 
-type numeric interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
-		~float32 | ~float64
-}
+func (r *Report) IsSafe(dampener bool) bool {
 
-func Abs[T numeric](val T) T {
-	if val < 0 {
-		return -val
-	}
-	return val
-}
-
-func (r *Report) IsSafe() bool {
-	increasing := r.Levels[1] > r.Levels[0]
-	for i, _ := range r.Levels {
-		if i == 0 {
-			continue
+	if !dampener && unsafeIdx(r.Levels) == -1 {
+		return true
+	} else if dampener {
+		for i := 0; i < len(r.Levels); i++ {
+			if unsafeIdx(deleteLevelAt(i, r.Levels)) == -1 {
+				return true
+			}
 		}
-		diff := Abs(r.Levels[i] - r.Levels[i-1])
-		isSequential := (increasing && r.Levels[i] > r.Levels[i-1]) || (!increasing && r.Levels[i] < r.Levels[i-1])
+	}
+	return false
+}
+
+func deleteLevelAt(idx int, levels []int) []int {
+	deleted := make([]int, len(levels)-1)
+	copy(deleted[:idx], levels[:idx])
+	copy(deleted[idx:], levels[idx+1:])
+	return deleted
+}
+
+func unsafeIdx(levels []int) int {
+	if len(levels) <= 1 {
+		return -1
+	}
+	increasing := levels[1] > levels[0]
+	for i := 1; i < len(levels); i++ {
+		diff := utils.Abs(levels[i] - levels[i-1])
+		isSequential := (increasing && levels[i] > levels[i-1]) || (!increasing && levels[i] < levels[i-1])
 		validDiff := 1 <= diff && diff <= 3
 		if !isSequential || !validDiff {
-			return false
+			return i
 		}
 	}
-	return true
+	return -1
 }
 
 func stringToIntSlice(input string) ([]int, error) {
@@ -124,18 +153,40 @@ func GetInput() ([]Report, error) {
 	return reports, nil
 }
 
+func part1(reports *[]Report) int {
+	safeReports := 0
+	useDampener := false
+	for _, report := range *reports {
+		if report.IsSafe(useDampener) {
+			safeReports++
+		}
+	}
+	return safeReports
+}
+
+func part2(reports *[]Report) int {
+	useDampener := true
+	safeReports := 0
+
+	for _, report := range *reports {
+		if report.IsSafe(useDampener) {
+			safeReports++
+		}
+	}
+	return safeReports
+}
+
 func main() {
 	reports, err := GetInput()
 	if err != nil {
 		log.Fatalf("GetInput: %v", err)
 	}
 
-	safeReports := 0
-	for _, report := range reports {
-		if report.IsSafe() {
-			safeReports++
-		}
-	}
+	fmt.Println("Part 1")
+	safeReports := part1(&reports)
+	fmt.Printf("Safe reports: %d\n", safeReports)
 
+	fmt.Println("Part 2")
+	safeReports = part2(&reports)
 	fmt.Printf("Safe reports: %d\n", safeReports)
 }
